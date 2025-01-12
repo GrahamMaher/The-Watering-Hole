667ff071896ae1657b62e36cc3c69128645db699
Graham Maher <mahergrahamsj@gmail.com>
2025-01-12 13:38:37 +0000

    Ready for deployment

--- the_watering_hole/settings.py
+++ the_watering_hole/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d1e49xtpc86h0%ptj=+y0ac9$!&0927o*dd4c2s%#d9q#0zyv-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '8000-grahammaher-thewatering-i7bv3udfxqx.ws-eu117.gitpod.io',
+    '.herokuapp.com'
+    ]
 
 
 # Application definition
