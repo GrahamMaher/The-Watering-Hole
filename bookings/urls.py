685397930c77c1287a1da3b364d4303f363a05e0
Graham Maher <mahergrahamsj@gmail.com>
2025-01-18 18:40:12 +0000

    add static files

--- bookings/urls.py
+++ bookings/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('reservations/', reservation_list, name='reservation_list'),
     path('reservations_make/', make_reservation, name='make_reservation'),
     path('reservations/modify/<int:reservation_id>', modify_reservation, name='modify_reservation'),
-    path('register/cancel/<int:reservation_id', cancel_reservation, name='cancel_reservation'),
+    path('register/cancel/<int:reservation_id>', cancel_reservation, name='cancel_reservation'),
 ]
--- the_watering_hole/urls.py
+++ the_watering_hole/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from bookings.views import restaurant_booking
+from django.urls import path, include
 
 urlpatterns = [
-    path('bookings/', restaurant_booking, name='bookings'),
+    path('', include('bookings.urls')),
     path('admin/', admin.site.urls),
 ]
